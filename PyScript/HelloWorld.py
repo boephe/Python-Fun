51d5b99f4d00c554c1632936fb59120520db3e99
boephe <phoebe@hurren.id.au>
2022-06-23 13:57:47 +0800

    just python
    get just some python working

--- PyScript/HelloWorld.py
+++ PyScript/HelloWorld.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+import matplotlib as plt
+import numpy as np
+
+print(plt.__version__)
+
+def wave(frequency, amplitude=1, phase=0):
+    def _wave(time):
+        return amplitude * np.sin(2 * np.pi * frequency * time + phase)
+
+    return _wave

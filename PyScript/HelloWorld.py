1ecb5ae8cde2256b69423eea84f2196b5ce1809e
boephe <phoebe@hurren.id.au>
2022-06-23 14:03:12 +0800

    the branch
    has this now got the code from the branch?

--- PyScript/HelloWorld.py
+++ PyScript/HelloWorld.py
@@ -1,3 +1,4 @@
+from ctypes import create_unicode_buffer
 import matplotlib as plt
 import numpy as np
 
@@ -8,3 +9,8 @@ def wave(frequency, amplitude=1, phase=0):
         return amplitude * np.sin(2 * np.pi * frequency * time + phase)
 
     return _wave
+
+
+    This is the new branch I create_unicode_buffer
+
+    but this is the code that is in the branch???
